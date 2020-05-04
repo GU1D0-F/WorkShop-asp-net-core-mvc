4fd4a41c30a8f6721a98604b8eb6536670125df0
Guilherme Fadel <gfadelt@gmail.com>
2020-05-04 14:41:29 -0300

    First Model-Controller-View - Department

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SalesWebMvc.Models;
 
+
 namespace SalesWebMvc.Controllers
 {
     public class HomeController : Controller
