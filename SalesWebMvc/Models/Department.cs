04cadd813dce3224ad0b1641745e2105d81f6163
Guilherme Fadel <gfadelt@gmail.com>
2020-05-04 14:48:25 -0300

    Deleting Department view and controller

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,9 +1,13 @@
-﻿namespace SalesWebMvc.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
     }
 }
