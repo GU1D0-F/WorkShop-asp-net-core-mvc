1cf0916057ebacf78850fc24a6d3fe91e5ab2353
Guilherme Fadel <gfadelt@gmail.com>
2020-05-05 13:32:55 -0300

    Foreing Key not null (Referential integrity)

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -22,6 +22,7 @@ namespace SalesWebMvc.Services
 
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First(); //Atribui o primeiro department ao seller
             _context.Add(obj);
             _context.SaveChanges();
         }
